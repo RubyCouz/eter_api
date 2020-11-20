e00a682e5248a2985684e35891d2f7f87ba66acd
Jat15 <jinan15@hotmail.com>
2020-11-20 12:17:06 +0100

    [Resolver] [Mutation] [EterUser] Create
    - Role utilisateur forcé pour tout ceux qui ne sont pas admin

--- src/Resolver/EterUserMutResolver.php
+++ src/Resolver/EterUserMutResolver.php
@@ -5,6 +5,7 @@ namespace App\Resolver;
 use ApiPlatform\Core\GraphQl\Resolver\MutationResolverInterface;
 use App\Entity\EterUser;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 
 final class EterUserMutResolver implements MutationResolverInterface
 {
@@ -15,10 +16,12 @@ final class EterUserMutResolver implements MutationResolverInterface
     */
 
     private $passwordEncoder;
+    private $security;
 
-    public function __construct( UserPasswordEncoderInterface $encoder)
+    public function __construct( UserPasswordEncoderInterface $encoder, Security $security)
     {
         $this->passwordEncoder = $encoder;
+        $this->security = $security; 
     }
 
     public function __invoke($item, array $context)
@@ -31,14 +34,16 @@ final class EterUserMutResolver implements MutationResolverInterface
 
         // The returned item will pe persisted.
 
-        $item->setUserRole("Utilisateur");
+        //dump($this->security->getUser());
+        //dump($this->security->getUser()->getUserRole());
+        //dump($this->security->isGranted('ROLE_ADMIN'));
+
+        if (!$this->security->isGranted('ROLE_ADMIN')) {
+            $item->setUserRole("Utilisateur");
+        }
 
         $item->setUserDesactivate(false);
-
-
-        //$dateinscr = strtotime('now');
         $item->setUserDate(new \DateTime('now'));
-        
         $password = $item->getUserPassword();
         $hash =  $this->passwordEncoder->encodePassword($item, $password);
         $item->setUserPassword($hash);
