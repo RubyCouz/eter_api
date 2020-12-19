0d7ff789242ba54044dfcf233ec27d13f69f80c1
Jat15 <jinan15@hotmail.com>
2020-12-19 15:11:58 +0100

    [Entity] [EterUser]  Security retiré pour collection_query

--- src/Entity/EterUser.php
+++ src/Entity/EterUser.php
@@ -50,9 +50,7 @@ use App\Resolver\EterUserMutResolver;
  *          "delete",
  *          "update",
  *          "item_query",
- *          "collection_query"={
- *              "security" = "is_granted('ROLE_ADMIN')",
- *          },
+ *          "collection_query",
  *          
  *      }
  * )
