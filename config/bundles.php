9b26f24455811ff563cabe4f9f6361e4b751fc4f
Jat15 <jinan15@hotmail.com>
2021-01-13 17:15:03 +0100

    [Composer] [Remove] Refresh Token

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
